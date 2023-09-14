4fb274d21514d0867b2acbeb0f4551f928664909
Sarabush12 <sarabushiro45@gmail.com>
2023-09-14 12:08:08 -0700

    -putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,17 @@
-int -putchar(char c);
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _lslower(int c);
+int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int _print_last_digit(int);
+int print_last_digit(int);
 void jack_bauer(void);
-void time_table(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_time_table(int n);
+
+#endif
